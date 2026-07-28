17fed2d9e3d2c0976b8d3474d32ac545cece44c9
Abhishek KR <abhishek636kr@gmail.com>
2026-07-28 10:25:14 +0530

    First weak completed on 28.07.2026

--- condition.py
+++ condition.py
@@ -1,70 +1,70 @@
-# # Conditions Statement
+# Conditions Statement
 
 
 
-# print("Conditions Statement")
-# A = int(input("Enter a number: "))
-# B = int(input("Enter another number: "))
-# C = int(input("Enter a third number: "))
-# D = int(input("Enter a fourth number: "))
+print("Conditions Statement")
+A = int(input("Enter a number: "))
+B = int(input("Enter another number: "))
+C = int(input("Enter a third number: "))
+D = int(input("Enter a fourth number: "))
 
-# if A < B and A < C and A < D:
-#     print("First value is less than all other values")
+if A < B and A < C and A < D:
+    print("First value is less than all other values")
     
-# elif B < C and B < D and B < A:
-#     print("Second value is less than all other values")
+elif B < C and B < D and B < A:
+    print("Second value is less than all other values")
     
-# elif C < A and C < B and C < D:
-#     print("Third value is less than all other values")
+elif C < A and C < B and C < D:
+    print("Third value is less than all other values")
     
-# elif D < A and D < B and D < C:
-#     print("Fourth value is less than all other values")
+elif D < A and D < B and D < C:
+    print("Fourth value is less than all other values")
     
-# else:
-#     print("No single value is less than all others")
+else:
+    print("No single value is less than all others")
 
-# print("----------------------------------------------------------------------------------------------------")
-
-
-
-# #nested if statement
-# print("Nested If Statement option 1")
-
-# N = int(input("Enter a number: "))
-# if N>=80:
-#     print("Grade A")
-#     if N>=95 and N<=100:
-#         print("Excellent")
-#     else:
-#         print("Good")
-# else:
-#     print("Grade B")
-#     if N>=70 and N<80:
-#         print("Average")
-#     else:
-#         print("Poor")       
+print("----------------------------------------------------------------------------------------------------")
 
 
-# print("---------------------------------------------------------------------------------------")
 
-# print("Nested If Statement option 2 with elif")
+#nested if statement
+print("Nested If Statement option 1")
+
+N = int(input("Enter a number: "))
+if N>=80:
+    print("Grade A")
+    if N>=95 and N<=100:
+        print("Excellent")
+    else:
+        print("Good")
+else:
+    print("Grade B")
+    if N>=70 and N<80:
+        print("Average")
+    else:
+        print("Poor")       
 
 
-# N = int(input("Enter a number: "))
-# if N>=80:
-#     print("Grade A")
-# elif N>=95 and N<=100:
-#     print("Excellent")
-# elif N>=70 and N<80:
-#     print("Average")
-# elif N<70:
-#     print("Grade B")
-# elif N<=60:  
-#     print("Grade C")    
-# elif N<50 and N>=40:
-#     print("poor")    
-# else:
-#     print("fail")              
+print("---------------------------------------------------------------------------------------")
+
+print("Nested If Statement option 2 with elif")
+
+
+N = int(input("Enter a number: "))
+if N>=80:
+    print("Grade A")
+elif N>=95 and N<=100:
+    print("Excellent")
+elif N>=70 and N<80:
+    print("Average")
+elif N<70:
+    print("Grade B")
+elif N<=60:  
+    print("Grade C")    
+elif N<50 and N>=40:
+    print("poor")    
+else:
+    print("fail")              
 
 
 #write a program for a simple calculator if statement
