4e658602e65a8a5d65695d6594e79468fc3c3cd0
Abhishek KR <abhishek636kr@gmail.com>
2026-09-12 11:33:26 +0530

    Update & clear the error in FizzBuzz.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,38 +6,35 @@ print("FizzBuzz")
 n = int(input("Enter the value of n : "))
 for i in range(1, n):
 
-        if i % 3 == 0 and i % 5 == 0:
-                print("FizzBuzz")
-        elif i % 3 == 0:
-         print("Fizz")
-        elif i % 5 == 0:
-                print("Buzz")
-else:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
         print(i)
+
 
 
 #write a promgram to find the n number is Armstrong number or not using for loop 
 
 
-
-
 n = int(input("Enter the value of n : "))
 
-        
 power = len(str(n))
 val = 0
 for i in str(n):
-                
-                val += int(i) ** power
+    val += int(i) ** power
 
-                if val == n:
-                        print(n, "is an Armstrong number")
-                else:
-                        print(n, "is not an Armstrong number")
-else:   
-        print("Please enter a valid number")        
+# check AFTER loop finishes
+if val == n:
+    print(n, "is an Armstrong number")
+else:
+    print(n, "is not an Armstrong number")
 
 
+    
 
 # Wirte  a program to find the n number is prime number or not using for loop
 
